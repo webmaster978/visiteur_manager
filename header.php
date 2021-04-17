adb96ad23907fecac27069953cb823728ed55fc5
webmaster978 <jtjoel11@gmail.com>
2021-04-18 00:19:13 +0200

    login

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
     <script type="text/javascript" src="js/bootstrap-datepicker1.js"></script>
     <link rel="stylesheet" type="text/css" href="css/datepicker.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 
--- index.php
+++ index.php
@@ -184,6 +184,91 @@ a {
 }
 </style>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+
+        </div>
+
+        <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+            <div class="card card0 border-0">
+                <div class="row d-flex">
+                    <div class="col-lg-6">
+                        <div class="card1 pb-5">
+                            <div class="row"> <img src="https://i.imgur.com/CXQmsmF.png" class="logo"> </div>
+                            <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img
+                                    src="https://i.imgur.com/uNGdWHi.png" class="image"> </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card2 card border-0 px-4 py-5">
+                            <div class="row mb-4 px-3">
+                                <h6 class="mb-0 mr-4 mt-2">Se connecter avec :</h6>
+                                <div class="facebook text-center mr-3">
+                                    <div class="fa fa-facebook"></div>
+                                </div>
+                                <div class="twitter text-center mr-3">
+                                    <div class="fa fa-twitter"></div>
+                                </div>
+                                <div class="linkedin text-center mr-3">
+                                    <div class="fa fa-linkedin"></div>
+                                </div>
+                                <div class="linkedin text-center mr-3">
+                                    <div class="fa fa-google"></div>
+                                </div>
+                            </div>
+                            <div class="row px-3 mb-4">
+                                <div class="line"></div> <small class="or text-center">Or</small>
+                                <div class="line"></div>
+                            </div>
+
+                            <form method="post" id="teacher_login_form" autocomplete="off">
+                                <div class="form-group">
+                                    <label>Votre adresse mail</label>
+                                    <input type="text" name="teacher_emailid" id="teacher_emailid" class="form-control"
+                                        required />
+                                    <span id="error_teacher_emailid" class="text-danger"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>Votre mot de passe </label>
+                                    <input type="password" name="teacher_password" id="teacher_password"
+                                        class="form-control" required />
+                                    <span id="error_teacher_password" class="text-danger"></span>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="teacher_login" id="teacher_login" class="btn btn-info"
+                                        value="Se connecter" />
+                                </div>
+                            </form>
+                            <div>
+                                <h4 class="text text-info" style="text-align: center;">Une solution pour la presence
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-blue py-4">
+                    <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; <?php $today = new DateTime('today');
+																								echo $today->format('2020 - Y'), PHP_EOL; ?>. All rights reserved by joel jt.</small>
+                        <div class="social-contact ml-4 ml-sm-auto"> <span class="fa fa-facebook mr-4 text-sm"></span>
+                            <span class="fa fa-google-plus mr-4 text-sm"></span> <span
+                                class="fa fa-linkedin mr-4 text-sm"></span> <span
+                                class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="col-md-4">
+
+        </div>
+    </div>
+</div>
+
 
 <div class="container">
     <h3 align="center">Visitor Management System</h3>
